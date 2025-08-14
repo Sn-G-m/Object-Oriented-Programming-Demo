fff25356680fc5f28311af088e6c7feffb51decc
Sn-G-m <112201012@smail.iitpkd.ac.in>
2025-08-14 14:17:59 +0530

    Communicator and diagrams

---
+++ Communicator/Communicator/RpcCommunicator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communicator
+{
+    public class RpcCommunicator : ICommunicator
+    {
+        public string Protocol = "RPC";
+        public RpcCommunicator() 
+        {
+            // Constructor logic
+        }
+
+        public void SendMessage(string message, string ipAddress)
+        {
+            RpcFunction1();
+            SendData(message, ipAddress);
+            RpcFunction2();
+        }
+
+        private void RpcFunction1()
+        {
+            Console.WriteLine("RPC Specific Function 1...");
+        }
+
+        private void RpcFunction2()
+        {
+            Console.WriteLine("RPC Specific Function 2...");
+        }
+
+        public void SendData(string data, string ipAddress)
+        {
+            Console.WriteLine($"Sending data to {ipAddress} via {Protocol}: {data}");
+        }
+
+        public string GetCommunicatorName()
+        {
+            return "RPC-Communicator";
+        }
+    }
+}
---
+++ Communicator/Controller/Program.cs
@@ -0,0 +1,32 @@
+﻿using Communicator;
+
+namespace Controller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the various communicator instances.
+            ICommunicator tcpCommunicator = new TcpCommunicator();
+            ICommunicator udpCommunicator = new UdpCommunicator();
+            ICommunicator rpcCommunicator = new RpcCommunicator();
+
+            // Create a list of communicators.
+            List<ICommunicator> communicators = new List<ICommunicator>();
+            communicators.Add(tcpCommunicator);
+            communicators.Add(udpCommunicator);
+            communicators.Add(rpcCommunicator);
+
+            // Create a list of messages.
+            List<string> messages = new List<string> { "Good Morning", "Buenos Dia", "Guten Tag" };
+
+            for (int i = 0; i < communicators.Count; i++)
+            {
+                Console.WriteLine($"Using {communicators[i].GetCommunicatorName()}");
+                communicators[i].SendMessage(messages[i], "127.0.0.1");
+                Console.WriteLine("Communication link closed\n");
+            }
+
+        }
+    }
+}
