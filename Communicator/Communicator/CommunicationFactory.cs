cdc7e0ece03e7b5e52d218b041b4722c24491c17
Sn-G-m <112201012@smail.iitpkd.ac.in>
2025-08-18 23:07:07 +0530

    Factory method implemented in Communicator

---
+++ Communicator/Communicator/CommunicationFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communicator
+{
+    public class CommunicationFactory
+    {
+        ICommunicator communicator = null;
+        public ICommunicator GetCommunicator(bool reliableRequired)
+        {   
+            if (reliableRequired == true)
+            { 
+                communicator = new TcpCommunicator();
+            }
+            else
+            {
+                communicator = new UdpCommunicator();
+            }
+            return communicator;
+        }
+    }
+}
--- Communicator/Controller/Program.cs
+++ Communicator/Controller/Program.cs
@@ -6,19 +6,27 @@ namespace Controller
     {
         static void Main(string[] args)
         {
+            // Create a reliable flag for TCP/UDP selection
+            bool reliableRequired = true;
+
             // Create the various communicator instances.
+            CommunicationFactory factory = new CommunicationFactory();
+            ICommunicator facTCommunicator = factory.GetCommunicator(reliableRequired);
+            ICommunicator facFCommunicator = factory.GetCommunicator(!reliableRequired);
             ICommunicator tcpCommunicator = new TcpCommunicator();
             ICommunicator udpCommunicator = new UdpCommunicator();
             ICommunicator rpcCommunicator = new RpcCommunicator();
 
             // Create a list of communicators.
             List<ICommunicator> communicators = new List<ICommunicator>();
+            communicators.Add(facTCommunicator);
+            communicators.Add(facFCommunicator);
             communicators.Add(tcpCommunicator);
             communicators.Add(udpCommunicator);
             communicators.Add(rpcCommunicator);
 
             // Create a list of messages.
-            List<string> messages = new List<string> { "Good Morning", "Buenos Dia", "Guten Tag" };
+            List<string> messages = new List<string> { "Good Day", "Buona Giornata", "Buenos Dia", "Guten Tag", "Dydd Da" };
 
             for (int i = 0; i < communicators.Count; i++)
             {
