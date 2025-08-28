9d833e4131f780aeb6b621b7de51d95b568b374f
Sn-G-m <112201012@smail.iitpkd.ac.in>
2025-08-28 23:19:19 +0530

    New networking project added implementing observer pattern and listener identifiers

---
+++ Networking/console-app/Executive/Program.cs
@@ -0,0 +1,25 @@
+﻿using Networking;
+using Screenshare;
+using Chat;
+
+namespace Executive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICommunicator communicator = new Communicator();
+            communicator.SendMessage("Test Message", "127.0.0.1");
+            Console.WriteLine("Message sent successfully.");
+
+            IMessageListener mainListener = new Listener();
+            IMessageListener chatListener = new ChatListener();
+            IMessageListener screenshareListener = new ScreenshareListener();
+            communicator.Subscribe("PRGM", mainListener);
+            communicator.Subscribe("CHAT", chatListener);
+            communicator.Subscribe("SCSR", screenshareListener);
+            Console.WriteLine("All Listeners subscribed successfully. Waiting for messages...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Networking/console-app/Networking/Listener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    public  class Listener :  IMessageListener
+    {
+        public void OnMessageReceived(string message)
+        {
+            Console.WriteLine($"Message Received : {message} [Receiver: PRGM]");
+        }
+    }
+}
